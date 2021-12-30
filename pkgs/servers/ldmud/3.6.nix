e83de6b271cba3bfe916d2386a77f2c2a3b77e6d
Daniel McCarney <daniel@binaryparadox.net>
2021-12-30 17:59:49 -0500

    wip: speed up build

--- pkgs/servers/ldmud/3.6.nix
+++ pkgs/servers/ldmud/3.6.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, bison }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, bison
+, pcreSupport ? true, pcre, gcryptSupport ? true, libgcrypt }:
 
 stdenv.mkDerivation rec {
   pname = "ldmud";
@@ -11,21 +12,54 @@ stdenv.mkDerivation rec {
     sha256 = "01jprn42s63kg54v9cr5a6ki08bhx2nqjygw6cw9y0abggb7qr9q";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkg-config bison ];
+  nativeBuildInputs = [ autoreconfHook pkg-config bison ]
+    ++ lib.optional pcreSupport pcre ++ lib.optional gcryptSupport libgcrypt;
 
   sourceRoot = "source/src";
+
+  enableParallelBuilding = true;
 
   postUnpack = ''
     cp $sourceRoot/autoconf/configure.ac $sourceRoot/
   '';
 
-  configureFlags = [ "--enable-use-pcre=no" "--enable-use-gcrypt=no" ];
+  # TODO:
+  #   - erq
+  #   - compat mode
+  #   - filename spaces
+  #   - ipv6
+  #   - mysql
+  #   - pgsql
+  #   - sqlite
+  #   - json
+  #   - xml
+  #   - deprecated
+  #   - tls
+  #   - python
+  #   - max configs
+  #     - malloc
+  #     - eval cost
+  #     - array size
+  #     - mapping keys
+  #     - mapping size
+  #     - max callouts
+  #     - max players
+  #     - max local
+  #     - hb interval
+  #     - alarm time
+  #     - time to reset
+  #
+  configureFlags = [
+    (lib.enableFeature pcreSupport "pcre")
+    (lib.enableFeature gcryptSupport "gcrypt")
+  ];
 
   installTargets = "install-driver install-utils install-headers";
 
   meta = with lib; {
     description =
       "LDMud is a gamedriver for LPMuds. (LPC compiler, interpreter and runtime environment.)";
+    #TODO - long description
     homepage = "https://ldmud.eu";
     license = licenses.bsd2; # TODO: This is only the license for new contribs.
     platforms = with platforms; linux; # TODO: Likely also freebsd?

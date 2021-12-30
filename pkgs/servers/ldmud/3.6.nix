5af10113539c1d6e9896138567c78d2f77c9d47f
Daniel McCarney <daniel@binaryparadox.net>
2021-12-30 17:22:04 -0500

    wip: first build

---
+++ pkgs/servers/ldmud/3.6.nix
@@ -0,0 +1,35 @@
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, bison }:
+
+stdenv.mkDerivation rec {
+  pname = "ldmud";
+  version = "3.6.4";
+
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = version;
+    sha256 = "01jprn42s63kg54v9cr5a6ki08bhx2nqjygw6cw9y0abggb7qr9q";
+  };
+
+  nativeBuildInputs = [ autoreconfHook pkg-config bison ];
+
+  sourceRoot = "source/src";
+
+  # Avoid using the LD autogen.sh wrapper - it just runs 'autoreconf' in the 'autoconf'
+  # subdir and moves the generated resources to src/ - we want to use the autoreconfHook
+  # that expects configure.ac to be in the $sourceRoot.
+  postUnpack = ''
+    cp $sourceRoot/autoconf/configure.ac $sourceRoot/
+  '';
+
+  configureFlags = [ "--enable-use-pcre=no" "--enable-use-gcrypt=no" ];
+
+  meta = with lib; {
+    description =
+      "LDMud is a gamedriver for LPMuds. (LPC compiler, interpreter and runtime environment.)";
+    homepage = "https://ldmud.eu";
+    license = licenses.bsd2; # TODO: This is only the license for new contribs.
+    platforms = with platforms; linux; # TODO: Likely also freebsd?
+    maintainers = with maintainers; [ cpu ];
+  };
+}
